b6f4797ca5ddcd83c961dbb52fff95130682921e
Vijay <ankittreo@gmail.com>
2024-07-31 16:33:12 +0530

    this is my first commit

---
+++ myfile.py
@@ -0,0 +1,24 @@
+def fibonacci_sequence(n):
+    """
+    Generate a Fibonacci sequence of n terms.
+    
+    :param n: Number of terms in the Fibonacci sequence to generate.
+    :return: A list containing the Fibonacci sequence.
+    """
+    sequence = []
+    a, b = 0, 1
+    for _ in range(n):
+        sequence.append(a)
+        a, b = b, a + b
+    return sequence
+
+# Define the number of terms in the sequence
+num_terms = 10
+
+# Generate the Fibonacci sequence
+fib_sequence = fibonacci_sequence(num_terms)
+
+# Print the Fibonacci sequence
+print(f"The first {num_terms} terms of the Fibonacci sequence are:")
+print(fib_sequence)
+
